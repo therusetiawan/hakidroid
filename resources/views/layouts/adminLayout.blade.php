17f8512a668de2edc6ed6977653ccc1fa2dcd277
Bintang Muhammad <bintangmhd@gmail.com>
2016-11-27 16:45:36 +0700

    navbar edit

--- resources/views/layouts/adminLayout.blade.php
+++ resources/views/layouts/adminLayout.blade.php
@@ -55,13 +55,11 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="/images/default.jpg" class="user-image" alt="User Image">
-              <span class="hidden-xs">Admin</span>
+              <span class="hidden-xs">Admin <i class="fa fa-caret-down pull-right"></i></span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="/images/default.jpg" class="img-circle" alt="User Image"/>
 
                 <p>
                   Admin
@@ -91,7 +89,7 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="/images/default.jpg" class="img-circle" alt="User Image">
+          <img src="{{asset('/img/batman.jpg')}}" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p>Admin
