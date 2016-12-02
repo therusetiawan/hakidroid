020d6d2db535c7444cd6fc20de09bfbb7c31a618
Bintang Muhammad <bintangmhd@gmail.com>
2016-12-02 20:07:12 +0700

    For Screenshots

--- resources/views/admin/pengajuanindustri.blade.php
+++ resources/views/admin/pengajuanindustri.blade.php
@@ -10,9 +10,7 @@
     <h3>Pengajuan Industri</h3>
   </div>
   <div class="content body">
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
+  
     <div class="row">
       <div class="col-xs-12">
         <div class="box box-success">
--- resources/views/admin/pengajuanpaten.blade.php
+++ resources/views/admin/pengajuanpaten.blade.php
@@ -9,9 +9,6 @@
     <h3>Pengajuan Paten</h3>
   </div>
   <div class="content body">
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
     <div class="row">
       <div class="col-xs-12">
         <div class="box box-success">
